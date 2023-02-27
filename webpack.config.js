9afdf3b7d172790c805798cc1a91ebb57359ac68
Kaleb Taylor <kaleb.taylor1976@gmail.com>
2023-02-26 16:56:56 -0800

    add Cleanwipe plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-// There's a new line below this one!
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
 
 module.exports = {
   entry: './src/index.js',
@@ -8,8 +8,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  // The plugins key below this line is also new!
   plugins: [
+    new CleanWebpackPlugin({
+      verbose: true
+    }) , // new line
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
